06b4bef8cc307f9e9f32741cc08a5e6c40b9a85b
Noah Tajwar <noah.tajwar.oss@gmail.com>
2019-03-10 14:50:59 -0700

    Re disabled angle wrapping due to comparison issues, added a harmonic oscillator like forcing

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from double_pendulum import *
 
-from math import sqrt
+from math import sin, cos, sqrt
 
-if __name__ == "__main__":
+class HarmonicOscillator(Potential):
 
+    def __init__(self, frequency: float):
+        self.__w = frequency
+
+    def U(self, state: DoublePendulum.State, prop: DoublePendulum.Properties) -> List[float]:
+        return [0, 0, 1/2*(self.__w**2)*(state.q()**4)]
+    
+    def dU_dcoord(self, state: DoublePendulum.State, prop: DoublePendulum.Properties) -> List[float]:
+        return [0, 0, 2*(self.__w**2)*state.q()**3]
+
+def run(theta1: float, theta2: float, behavior: DoublePendulumBehavior):
     # Setup pendulum
     L = 1            # m
     m = 2            # kg
     d = sqrt(1/12)*L # m
 
     pendulum = DoublePendulum(DoublePendulum.Properties(L, m, d), DoublePendulum.State(
-        theta1     = pi/10,
-        theta2     = 0,
+        theta1     = theta1,
+        theta2     = theta2,
         q          = 0,
         theta1_dot = 0,
         theta2_dot = 0,
@@ -19,7 +29,7 @@ if __name__ == "__main__":
     ))
 
     # Choose behavior
-    behavior = GeneralDoublePendulumBehavior(SumPotential(ZeroPotential(), FixedQPotential()))
+    behavior = GeneralDoublePendulumBehavior(behavior)
 
     # Setup solvers
     time_evolver = ODEINTTimeEvolver()
@@ -33,3 +43,6 @@ if __name__ == "__main__":
     animator = DoublePendulumAnimator(simulation)
     animator.init()
     animator.run(dt, draw_dt, 1000)
+
+if __name__ == "__main__":
+    run(-pi/100, pi/100, HarmonicOscillator(50))
