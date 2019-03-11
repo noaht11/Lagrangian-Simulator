a20985e6434079e20fc3ab35b5d463453600b5e9
Noah Tajwar <noah.tajwar.oss@gmail.com>
2019-03-10 17:21:55 -0700

    Balancing for about 1.5 seconds

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from double_pendulum import *
 
 from math import sin, cos, sqrt
 
-def run(theta1: float, theta2: float, behavior: DoublePendulumBehavior):
+def run(theta1: float = 0, theta2: float = 0, q: float = 0, potential: Potential = ZeroPotential()):
     # Setup pendulum
     L = 1            # m
     m = 2            # kg
@@ -11,14 +11,14 @@ def run(theta1: float, theta2: float, behavior: DoublePendulumBehavior):
     pendulum = DoublePendulum(DoublePendulum.Properties(L, m, d), DoublePendulum.State(
         theta1     = theta1,
         theta2     = theta2,
-        q          = 0,
+        q          = q,
         theta1_dot = 0,
         theta2_dot = 0,
         q_dot      = 0
     ))
 
     # Choose behavior
-    behavior = GeneralDoublePendulumBehavior(behavior)
+    behavior = GeneralDoublePendulumBehavior(potential)
 
     # Setup solvers
     time_evolver = ODEINTTimeEvolver()
@@ -28,10 +28,10 @@ def run(theta1: float, theta2: float, behavior: DoublePendulumBehavior):
     dt      = 1.0 / 50
     draw_dt = 1.0 / 50
 
-    # Run animation
+    # Run animated simulation
     animator = DoublePendulumAnimator(simulation)
     animator.init()
     animator.run(dt, draw_dt, 1000)
 
 if __name__ == "__main__":
-    run(pi/10, pi/10, FixedQPotential())
+    run(theta1 = pi/100, theta2 = pi/100, q = 0, potential = HarmonicOscillatorPotential(q_k = 1.2358011, q_eq = 1))
