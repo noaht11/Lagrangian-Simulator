c58013c69f6b58349b15cfe30b759aebf0b093d7
Noah Tajwar <noah.tajwar.oss@gmail.com>
2019-03-25 03:06:12 -0700

    Started new Pendulum class

--- pendulum/__init__.py
+++ pendulum/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["core", "behavior", "potential", "simulate", "animate"]
+__all__ = ["core"]
--- pendulum/core.py
+++ pendulum/core.py
@@ -1,170 +1,185 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Callable, Dict
 
+import sympy as sp
+
 from math import sin, cos, pi, inf, exp
 
 ###################################################################################################################################################################################
 # UTILITY FUNCTIONS
 ###################################################################################################################################################################################
 
-# Converts an angle in radians to the equivalent angle in radians constrained between -pi and pi, where 0 remains at angle 0
 def neg_pi_to_pi(theta: float) -> float:
+    """Converts an angle in radians to the equivalent angle in radians constrained between -pi and pi, where 0 remains at angle 0
+    """
     modded = theta % (2*pi)
     return modded + (modded > pi) * (-2*pi)
 
 ###################################################################################################################################################################################
-# DOUBLE PENDULUM DEFINITION
+# PENDULUM DEFINITION
 ###################################################################################################################################################################################
 
-# Class representing the instantaneous state of a double pendulum with at most 3 degrees of freedom:
-#
-#   1.  theta1  (angle between the first arm and the vertical, 0 = straight up from the pivot point)
-#   2.  theta2  (angle between the second arm and the vertical, 0 = straight up from the joint)
-#   3.  q       (horizontal position of the pivot point relative to an arbitrary origin)
-#
-# The state of the double pendulum is contained in an array of six values (in the following order):
-#
-#   [0]    =   theta1
-#   [1]    =   theta2
-#   [2]    =   q
-#   [3]    =   theta1_dot   (time derivative of theta1)
-#   [4]    =   theta2_dot   (time derivative of theta2)
-#   [5]    =   q_dot        (time derivative of q)
-#
-class DoublePendulum:
+class Pendulum:
+    """Represents the properties, math, and state of a single pendulum
 
-    # Immutable class representing the current state of a double pendulum.
-    #
-    # See the DoublePendulum class description for information about each parameter.
-    #
-    class State:
-        def __init__(self, theta1: float = 0, theta2: float = 0, q: float = 0, theta1_dot: float = 0, theta2_dot: float = 0, q_dot: float = 0):
-            self.__theta1     = theta1
-            self.__theta2     = theta2
-            self.__q          = q
-            self.__theta1_dot = theta1_dot
-            self.__theta2_dot = theta2_dot
-            self.__q_dot      = q_dot
-        
-        def theta1(self)     -> float : return self.__theta1
-        def theta2(self)     -> float : return self.__theta2
-        def q(self)          -> float : return self.__q
-        def theta1_dot(self) -> float : return self.__theta1_dot
-        def theta2_dot(self) -> float : return self.__theta2_dot
-        def q_dot(self)      -> float : return self.__q_dot
-        
-        def __str__(self):
-                return str([self.theta1(), self.theta2(), self.q(), self.theta1_dot(), self.theta2_dot(), self.q_dot()])
+    The state of the pendulum is represented numerically while the math governing the energies of the pendulum
+    is represented symbolically using SymPy expressions.
+    This allows the Lagrangian equations of motion to be simplified symbolically before being integrated numerically.
+
+    Attributes:
+        `prop`  : attributes of the pendulum that do not change over time
+        `sym`   : symbolic quantities required to define the Lagrangian equations for the pendulum
+        `state` : numeric values required to define the current instantaneous motion of the pendulum
+    """
     
-    # Immutable class representing the mechanical properties of a double pendulum.
     class Properties:
-        # Initializes a new set of properties with the provided values
-        #
-        #   L             =   length of each arm
-        #   m             =   mass of each arm
-        #   d             =   moment of inertia factor such that I = m * d^2
-        #
-        def __init__(self, L: float = 1.0, m: float = 1.0, d: float = 1.0, **kwargs):
-            self.__L = L
-            self.__m = m
-            self.__d = d
-            self.__extras = kwargs
+        """Represents all attributes of the pendulum that do not change over time:
+
+        Attributes:
+            `L`      : length of the pendulum
+            `m`      : total mass of the pendulum
+            `I`      : moment of inertia of the pendulum about an axis passing through its center, perpendicular to the plane of oscillation
+            `extras` : additional, instance-specific properties
+        """
+
+        def __init__(self, L: float, m: float, I: float, **extras):
+            self._L = L
+            self._m = m
+            self._I = I
+            self._extras = extras
         
-        def L(self) -> float: return self.__L
-        def m(self) -> float: return self.__m
-        def d(self) -> float: return self.__d
-        def extras(self) -> Dict[str, float]: return self.__extras
+        @property
+        def L(self) -> float: return self._L
+        @property
+        def m(self) -> float: return self._m
+        @property
+        def I(self) -> float: return self._I
+        @property
+        def extras(self) -> Dict[str, float]: return self._extras
 
-    # Initializes a new DoublePendulum with the provided properties and initial state
-    #
-    #   prop          =   mechanical properties of the pendulum
-    #   init_state    =   initial state of the pendulum
-    #
-    def __init__(self, prop: Properties = Properties(), init_state: State = State()):
-        self.__prop = prop
-        self.__state = init_state
+    class Symbols:
+        """Represents the symbolic quantities required to define the Lagrangian equations for the pendulum:
+
+        Attributes:
+            `x`     : x coordinate of the endpoint of the pendulum, closest to the first element in a chain of pendulums if applicable
+            `y`     : y coordinate of the endpoint of the pendulum, closest to the first element in a chain of pendulums if applicable
+            `theta` : angle of the pendulum with respect to the vertical through (x,y)
+        """
+
+        def __init__(self, x: sp.Expr, y: sp.Expr, theta: sp.Symbol):
+            self._x = x
+            self._y = y
+            self._theta = theta
+        
+        @property
+        def x(self)     -> sp.Expr   : return self._x
+        @property
+        def y(self)     -> sp.Expr   : return self._y
+        @property
+        def theta(self) -> sp.Symbol : return self._theta
+
+    class State:
+        """Holds the numeric values required to define the current instantaneous motion of the pendulum
+
+        Attributes:
+            `theta`     : angle of the pendulum with respect to the vertical through (x,y)
+            `theta_dot` : anglular velocity of the pendulum about an axis on the pendulum, perpendicular to the plane of oscillation
+        """
+
+        def __init__(self, theta: float = 0, theta_dot: float = 0):
+            self._theta     = theta
+            self._theta_dot = theta_dot
+        
+        @property
+        def theta(self)     -> float : return self._theta
+        @property
+        def theta_dot(self) -> float : return self._theta_dot
+        
+        def __str__(self) -> str:
+                return str([self.theta(), self.theta_dot()])
+
+    def __init__(self, prop: Properties, sym: Symbols, state: State):
+        self._prop = prop
+        self._sym = sym
+        self._state = state
     
-    # Returns the mechanical properties of the pendulum
-    def prop(self) -> Properties:
-        return self.__prop
+    @property
+    def prop(self)  -> Properties: return self._prop
+    @property
+    def sym(self)   -> Symbols:    return self._sym
+    @property
+    def state(self) -> State:      return self._state
 
-    # Returns the current state
-    def state(self) -> State:
-        return self.__state
-
-    # Sets the current state to new_state
     def set_state(self, new_state: State):
-        # # Constrain the angles to between -pi and pi for easy interpretation
-        adj_state = DoublePendulum.State(
-            neg_pi_to_pi(new_state.theta1()),
-            neg_pi_to_pi(new_state.theta2()),
-            new_state.q(),
-            new_state.theta1_dot(),
-            new_state.theta2_dot(),
-            new_state.q_dot()
-        )
-        self.__state = adj_state
-
-    # Calculates and returns the positions of the endpoints of the arms of the pendulum
-    #
-    # The return value is a tuple of the following form:
-    #   (
-    #       (pivot_x, pivot_y),
-    #       (joint_x, joint_y),
-    #       (end_x  , end_y  )
-    #   )
-    def position_ends(self) -> Tuple[Tuple[float, float], Tuple[float, float], Tuple[float, float]]:
-        # Local copies of the state variables for convenience
-        L = self.prop().L()
-        theta1 = self.state().theta1()
-        theta2 = self.state().theta2()
-        q = self.state().q()
+        """Updates the current state of the pendulum
         
-        x_1 = q + L*sin(theta1)
-        y_1 = L*cos(theta1)
+        Arguments:
+            `new_state` : the new state of the pendulum
+        """
 
-        x_2 = q + L*(sin(theta1) + sin(theta2))
-        y_2 = L*(cos(theta1) + cos(theta2))
+        # Constrain the angles to between -pi and pi for easy interpretation
+        adj_state = Pendulum.State(
+            neg_pi_to_pi(new_state.theta()),
+            new_state.theta_dot()
+        )
+        self._state = adj_state
 
-        return ((q, 0.0), (x_1, y_1), (x_2, y_2))
-    
-    # Calculates and returns the positions of the pivot point and COM (centre of mass) of each arm of the pendulum
-    #
-    # The return value is a tuple of the following form:
-    #   (
-    #       (pivot_x, pivot_y),
-    #       (COM_1_x, COM_1_y),
-    #       (COM_2_x, COM_2_y)
-    #   )
-    def position_COMs(self) -> Tuple[Tuple[float, float], Tuple[float, float], Tuple[float, float]]:
-        # Local copies of the state variables for convenience
-        L = self.prop().L()
-        theta1 = self.state().theta1()
-        theta2 = self.state().theta2()
-        q = self.state().q()
+    def start(self) -> Tuple[sp.Expr, sp.Expr]:
+        """Calculates expressions for the coordinates of the start endpoint of the pendulum
+        
+        Returns: Tuple of (x, y) where each coordinate is a symbolic expression
+        """
+        return (self.sym.x, self.sym.y)
 
-        x_1 = q + L/2*sin(theta1)
-        y_1 = L/2*cos(theta1)
+    def com(self) -> Tuple[sp.Expr, sp.Expr]:
+        """Calculates expressions for the coordinates of the centre of mass of the pendulum
 
-        x_2 = q + L*(sin(theta1) + 1/2*sin(theta2))
-        y_2 = L*(cos(theta1) + 1/2*cos(theta2))
+        Returns: Tuple of (x_COM, y_COM) where each coordinate is a symbolic expression
+        """
+        L = self.prop.L
+        theta = self.sym.theta
 
-        return ((q, 0.0), (x_1, y_1), (x_2, y_2))
+        x_com = self.sym.x + L/2 * sp.sin(theta)
+        y_com = self.sym.y + L/2 * sp.cos(theta)
 
-    # Calculates and returns the kinetic energy of the current state of the pendulum
-    def energy_kinetic(self) -> float:
-        # Local copies of the state variables for convenience
-        L = self.prop().L()
-        m = self.prop().m()
-        d = self.prop().d()
-        theta1 = self.state().theta1()
-        theta2 = self.state().theta2()
-        q = self.state().q()
-        theta1_dot = self.state().theta1_dot()
-        theta2_dot = self.state().theta2_dot()
-        q_dot = self.state().q_dot()
+        return (x_com, y_com)
 
-        # This is just the final formula for kinetic energy after a lot of re-arranging, so there's no good way to decompose it
-        T = 1/2*m * (2*q_dot**2   +   q_dot*L*(3*cos(theta1)*theta1_dot + cos(theta2)*theta2_dot)   +   L**2*(1/4*(5*theta1_dot**2 + theta2_dot**2) + theta1_dot*theta2_dot*cos(theta1 - theta2))   +   d**2*(theta1_dot**2 + theta2_dot**2))
+    def end(self) -> Tuple[sp.Expr, sp.Expr]:
+        """Calculates expressions for the coordinates of the end endpoint of the pendulum
 
-        return T
+        Returns: Tuple of (x_end, y_end) where each coordinate is a symbolic expression
+        """
+        L = self.prop.L
+        theta = self.sym.theta
+
+        x_end = self.sym.x + L * sp.sin(theta)
+        y_end = self.sym.y + L * sp.cos(theta)
+
+        return (x_end, y_end)
+
+    def energy_potential(self) -> sp.Expr:
+        L = self.prop.L
+        m = self.prop.m
+
+        return m * L / 2 * sp.cos(self.sym.theta)
+
+    def energy_kinetic(self) -> sp.Expr:
+        pass
+
+    # # Calculates and returns the kinetic energy of the current state of the pendulum
+    # def energy_kinetic(self) -> float:
+    #     # Local copies of the state variables for convenience
+    #     L = self.prop().L()
+    #     m = self.prop().m()
+    #     d = self.prop().d()
+    #     theta1 = self.state().theta1()
+    #     theta2 = self.state().theta2()
+    #     q = self.state().q()
+    #     theta1_dot = self.state().theta1_dot()
+    #     theta2_dot = self.state().theta2_dot()
+    #     q_dot = self.state().q_dot()
+
+    #     # This is just the final formula for kinetic energy after a lot of re-arranging, so there's no good way to decompose it
+    #     T = 1/2*m * (2*q_dot**2   +   q_dot*L*(3*cos(theta1)*theta1_dot + cos(theta2)*theta2_dot)   +   L**2*(1/4*(5*theta1_dot**2 + theta2_dot**2) + theta1_dot*theta2_dot*cos(theta1 - theta2))   +   d**2*(theta1_dot**2 + theta2_dot**2))
+
+    #     return T
